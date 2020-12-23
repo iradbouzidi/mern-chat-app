145698634734d3d3dbb4de0e2acba610cc948439
iradbouzidi <irad.bouzidi@esprit.tn>
2020-12-24 00:04:55 +0100

    update

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
+const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-const http = require("http");
+const cors = require("cors");
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
-
-const PORT = process.env.PORT || 4000;
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const router = require("./router");
 
@@ -12,23 +11,29 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on("connection", (socket) => {
+app.use(cors());
+app.use(router);
+
+io.on("connect", (socket) => {
   socket.on("join", ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
     if (error) return callback(error);
 
+    socket.join(user.room);
+
     socket.emit("message", {
       user: "admin",
-      text: `${user.name}, Welcome to the room ${user.room}.`,
+      text: `${user.name}, welcome to room ${user.room}.`,
     });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
 
-    socket.broadcast.to(user.room).emit("message", {
-      user: "admin",
-      text: `${user.name} has joined!`,
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
     });
-
-    socket.join(user.room);
 
     callback();
   });
@@ -42,10 +47,21 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log("User had left!!!");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
   });
 });
 
-app.use(router);
-
-server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+server.listen(process.env.PORT || 4000, () =>
+  console.log(`Server has started.`)
+);
